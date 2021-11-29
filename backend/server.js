0f28ce984b82a240c795a184c51d3a945351b9b4
Johnson Olaoluwa <johnsonolaolu@gmail.com>
2021-11-29 13:42:24 +0100

    setup file structure

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.post("/" , (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
+
+app.listen(5000)
+
+
